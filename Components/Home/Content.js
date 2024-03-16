7bd1ebb0dc9a96a76d62989ed3d67e25ad226673
AdarshNirnakar <adarshnirnakar2002@gmail.com>
2024-03-16 16:49:49 +0530

    Ontent.js completed

--- Components/Home/Content.js
+++ Components/Home/Content.js
@@ -1,12 +1,53 @@
-import { View, Text } from 'react-native'
+import { View,StyleSheet, Text } from 'react-native'
 import React from 'react'
 
 const Content = () => {
   return (
-    <View>
-      <Text>Title</Text>
+    <View style={styles.Container}>
+      
+      <View style={styles.boxGetHelp}>
+        <Text style={styles.Text}>get help</Text> 
+      </View>
+
+      
+
+      <View style={styles.boxHelpOthers}>
+        <Text style={styles.Text}>help others</Text> 
+      </View>
+
+      
+
     </View>
   )
 }
+const styles = StyleSheet.create({
+  Container:{
+    height:'87%',
+    alignItems:'stretch',
+    justifyContent:'space-evenly',
+    
+    
+  },
+
+  boxGetHelp:{
+    padding:50,
+    backgroundColor:'#9B64E1',
+    borderRadius:25,
+    margin:50,
+    marginBottom:-40
+    
+  },
+  boxHelpOthers:{
+    padding:50,
+    backgroundColor:'#9B64E1',
+    borderRadius:25,
+    margin:50,
+    marginTop:-40,
+  },
+  Text:{
+    fontSize:25
+  }
+
+})
 
 export default Content
