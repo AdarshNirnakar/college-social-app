decb048cdd0b91c542acef44e5c64638c636358c
AdarshNirnakar <adarshnirnakar2002@gmail.com>
2024-03-19 20:32:21 +0530

    button working

--- frontend/App.js
+++ frontend/App.js
@@ -1,15 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import {  StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 import HomeScreen from './Components/screens/HomeScreen';
+import HelpOthers from './Components/screens/HelpOthers';
+import GetHelpScreen from './Components/screens/GetHelpScreen';
+import ChatScreen from './Components/screens/ChatScreen';
+import Profile from './Components/screens/Profile';
+import NotificationScreen from './Components/screens/NotificationScreen';
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View >
-      <StatusBar style="auto" />
-      <HomeScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Notifications" component={NotificationScreen} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="HelpOthers" component={HelpOthers} />
+        <Stack.Screen name="GetHelpScreen" component={GetHelpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    
     
   );
 }
--- frontend/Components/Home/Content.js
+++ frontend/Components/Home/Content.js
@@ -1,32 +1,46 @@
-import { View,StyleSheet, Text, ScrollView } from 'react-native'
+import { View, StyleSheet, Text, ScrollView, Button } from 'react-native'
 import React from 'react'
 import GlobalStyles from '../styles/GlobalStyles'
+import { useNavigation } from "@react-navigation/native";
 
 
 const Content = () => {
+
+  const navigation = useNavigation();
+
+
+
   return (
     <>
-    <View style={styles.Container}>
-      
-      <View style={GlobalStyles.button}>
+      <View style={styles.Container}>
+
+        <View style={GlobalStyles.button}>
+          <Button title='profile' onPress={() => navigation.navigate("Profile")}>
+          </Button>
+
+        </View>
+
+
+
+        {/* <View style={GlobalStyles.button}>
         <Text className ='text-lg'>get help</Text> 
-      </View>
+      </View> */}
 
-      <View style={GlobalStyles.button}>
-        <Text style={GlobalStyles.Text}>help others</Text> 
-      </View>
+        <View style={GlobalStyles.button}>
+          <Text style={GlobalStyles.Text}>help others</Text>
+        </View>
 
-    </View>
-    <View style={GlobalStyles.Seperator}></View>
+      </View>
+      <View style={GlobalStyles.Seperator}></View>
     </>
-    
+
   )
 }
 const styles = StyleSheet.create({
-  Container:{
-    height:'80%',
-    alignItems:'stretch',
-    justifyContent:'center',
+  Container: {
+    height: '80%',
+    alignItems: 'stretch',
+    justifyContent: 'center',
   },
 })
 
