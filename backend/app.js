e65997ac95d13544979f9d567ef4a50b6491d430
EffDuBois <arnabkar2003@protonmail.com>
2024-05-11 21:56:37 +0530

    Refactor - auth

--- backend/app.js
+++ backend/app.js
@@ -4,29 +4,9 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 const cors = require("cors");
-
-const admin = require("firebase-admin");
+const { default: checkAuth } = require("./services/fbAuth");
 
 var app = express();
-
-const serviceAccount = require("config/fbServiceAccountKey.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "", // db url
-});
-
-function checkAuth(req, res, next) {
-  if (req.headers.authtoken) {
-    admin
-      .auth()
-      .verifyIdToken(req.headers.authtoken)
-      .then(() => next())
-      .catch(() => res.status(403).send("Unauthorised"));
-  } else {
-    res.status(403).send("Unauthorised");
-    return;
-  }
-}
 
 app.use(logger("dev"));
 app.use(express.json());
